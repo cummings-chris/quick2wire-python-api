f01a7fe6c22e462ee648c2ed9466fc35998b274f
Chris Cummings <19612665+cummings-chris@users.noreply.github.com>
2018-11-24 14:03:20 -0600

    Update board_revision.py

--- quick2wire/board_revision.py
+++ quick2wire/board_revision.py
@@ -3,6 +3,7 @@ def revision():
         with open('/proc/cpuinfo','r') as f:
             for line in f:
                 if line.startswith('Revision'):
+# This line needs to be fixed to detect the new rPi 3 B+
                     return 1 if line.rstrip()[-1] in ['2','3'] else 2
             else:
                 return 0
